43d22c3faba4850e8b24b6ee588a8bd57d8ae851
siddharthoak <sidoak@gmail.com>
2020-10-28 05:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    file1 = open('/opt/app-root/secure/myapp.sec', 'r') 
+    Lines = file1.readlines() 
+  
+    count = 0
+    # Strips the newline character 
+    for line in Lines: 
+    print("Secret Line{}: {}".format(count, line.strip())) 
+
     if WHO:
         return "Hello {WHO} ........".format(WHO)
     return 'Hello World!!'
