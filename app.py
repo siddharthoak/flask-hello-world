d07708f3439f76399e14e9733408138c2db45009
siddharthoak <sidoak@gmail.com>
2020-10-28 05:46:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
+import os
+
+WHO = os.environ['WHO']
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    if WHO:
+        return "Hello {WHO} ........".format(WHO)
     return 'Hello World!!'
 
 if __name__ == '__main__':
