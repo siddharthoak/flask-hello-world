5017a07edb8130757fe6c2322d5aefb99ca830ed
siddharthoak <sidoak@gmail.com>
2020-11-02 05:57:25 +0530

    Add WHO

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
-import os
-import sys 
-WHO = os.environ['WHO']
 
 
 app = Flask(__name__)
+WHO = os.environ['WHO']
 
 @app.route('/')
-def hello_world():        
-    file1 = open('/opt/app-root/secure/myapp.sec', 'r') 
-    Lines = file1.readlines() 
-  
-    count = 0
-    # Strips the newline character 
-    op = ""
-    for line in Lines: 
-        op += "Secret Line{}: {}".format(count, line.strip())
-        
+def hello_world():      
     if WHO:
-        if op:
-            return op
-        else:
-            return WHO
+        return WHO
     else:
         return 'Hello World!!'
-
+      
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
     
